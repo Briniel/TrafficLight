3b080dd0ef49f671df3c738d31f7e28cee4bff99
Briniel <lord.devol@gmail.com>
2021-08-17 11:35:29 +0300

    Создание главного экрана приложения. Реализован светофор, где после запуска приложения отображается выключенные светофор с кнопкой "Start". При нажатии на кнопку "Start" меняется ее название на "NEXT" и загорается красный свет. При дальнейшем нажатии на кнопку "Next" Переключается сигнал светофора

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var viewRedLight: UIView!
+    @IBOutlet var viewYellowLight: UIView!
+    @IBOutlet var viewGreenLight: UIView!
+    @IBOutlet var buttonStart: UIButton!
+
+    let radius: CGFloat = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewRedLight.layer.cornerRadius = radius
+        viewYellowLight.layer.cornerRadius = radius
+        viewGreenLight.layer.cornerRadius = radius
+        buttonStart.layer.cornerRadius = 10
     }
 
-
+    @IBAction func clickButtonStart() {
+        if viewRedLight.alpha == 1 {
+            viewRedLight.alpha = 0.3
+            viewYellowLight.alpha = 1
+        } else if viewYellowLight.alpha == 1 {
+            viewYellowLight.alpha = 0.3
+            viewGreenLight.alpha = 1
+        } else if viewGreenLight.alpha == 1 {
+            viewGreenLight.alpha = 0.3
+            viewRedLight.alpha = 1
+        } else {
+            buttonStart.setTitle("Next", for: .normal)
+            viewRedLight.alpha = 1
+        }
+    }
 }
 
